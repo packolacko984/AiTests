249551639974215f7e291fa15491098a0c5c306c
packolacko984 <idrissimahdi2022@gmail.com>
2025-04-24 20:19:05 +0100

    Update traffic.js

--- traffic.js
+++ traffic.js
@@ -343,9 +343,9 @@ const tasksPoll = async (views) => {
     () => {
       let location = locations[generateRandomNumber(0, locations.length + 1)];
       const username =
-        "qualityser-res-" +
+        "user-doublemobmedia-sessionduration-10-country-" +
         location +
-        "-sid-" +
+        "-session-" +
         String(generateRandomNumber(10000, 10000000));
 
       return OpenBrowser(url ? url : "https://www.google.com", username);
